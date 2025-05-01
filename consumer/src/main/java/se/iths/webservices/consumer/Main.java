14befd9bfdde8c353b5aea4295931c60fc7de491
VictoriaNaenfeldtEklund <victoria.naenfeldt.eklund@iths.se>
2025-05-01 17:05:04 +0200

    Refactor deliveryServiceMap to have a String key instead of integer for better compatibility with scanner.nextLine().

--- consumer/src/main/java/se/iths/webservices/consumer/Main.java
+++ consumer/src/main/java/se/iths/webservices/consumer/Main.java
@@ -9,18 +9,18 @@ public class Main {
     public static void main(String[] args) {
 
         ServiceLoader<DeliveryService> deliveryServiceLoader = ServiceLoader.load(DeliveryService.class);
-        Map<Integer, DeliveryService> deliveryServiceMap = createDeliveryServiceMapWithIdKey(deliveryServiceLoader);
+        Map<String, DeliveryService> deliveryServiceMap = createDeliveryServiceMapWithIdKey(deliveryServiceLoader);
         DeliveryService deliveryService = getUserChoiceOfDeliveryService(deliveryServiceMap);
         System.out.printf("\nYour order will be sent with delivery company:\nName: %-20s Price: %6s kr", deliveryService.getName(), deliveryService.getPrice().toString());
     }
 
-    private static Map<Integer, DeliveryService> createDeliveryServiceMapWithIdKey(ServiceLoader<DeliveryService> deliveryServiceLoader) {
+    private static Map<String, DeliveryService> createDeliveryServiceMapWithIdKey(ServiceLoader<DeliveryService> deliveryServiceLoader) {
 
-        Map<Integer, DeliveryService> deliveryServiceMap = new HashMap<>();
+        Map<String, DeliveryService> deliveryServiceMap = new HashMap<>();
         int id = 1;
 
         for(DeliveryService deliveryService : deliveryServiceLoader) {
-            deliveryServiceMap.put(id, deliveryService);
+            deliveryServiceMap.put(Integer.toString(id), deliveryService);
             id++;
         }
 
@@ -31,28 +31,23 @@ public class Main {
         return deliveryServiceMap;
     }
 
-    private static DeliveryService getUserChoiceOfDeliveryService(Map<Integer, DeliveryService> deliveryServiceMap) {
+    private static DeliveryService getUserChoiceOfDeliveryService(Map<String, DeliveryService> deliveryServiceMap) {
 
         Scanner scanner = new Scanner(System.in);
 
         while(true){
 
-            try {
+            System.out.println("\nPlease choose a delivery company for your order:");
+            deliveryServiceMap.forEach((id, service) -> System.out.printf("\n%s. Name: %-20s Price: %6s kr", id, service.getName(), service.getPrice().toString()));
+            System.out.print("\nEnter the number of your choice: ");
+            DeliveryService deliveryService = deliveryServiceMap.get(scanner.nextLine());
 
-                System.out.println("\nPlease choose a delivery company for your order:");
-                deliveryServiceMap.forEach((id, service) -> System.out.printf("\n%d. Name: %-20s Price: %6s kr", id, service.getName(), service.getPrice().toString()));
-                System.out.print("\nEnter the number of your choice: ");
-                DeliveryService deliveryService = deliveryServiceMap.get(Integer.parseInt(scanner.nextLine()));
-
-                if(deliveryService == null) {
-                    throw new NullPointerException();
-                }
-
-                return deliveryService ;
-
-            } catch (NumberFormatException | NullPointerException | ClassCastException e) {
-                System.out.println("\nPlease enter a valid choice for your delivery service.");
+            if(deliveryService != null) {
+                scanner.close();
+                return deliveryService;
             }
+
+            System.out.println("\nPlease enter a valid choice for your delivery service.");
         }
     }
 
